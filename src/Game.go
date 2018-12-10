5f4c1f4c80255f3980b1d306e96b28960c60c7e6
Jason Quinn <quinnjb23@gmail.com>
2018-12-10 14:11:00 -0700

    Added keyboard listener to Game.go

--- src/Game.go
+++ src/Game.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"log"
-
+	termbox "github.com/nsf/termbox-go"
 	"github.com/gorilla/websocket"
 )
 
@@ -22,5 +22,32 @@ func handlePosition(pos position) {
 			delete(clients, client)
 		}
 
+	}
+}
+
+func handleInput() {
+	var pos = position(X: 100, Y: 100)
+	for {
+		// if any of the keyboard events are captured
+		select {
+		case ev := <-events:
+			if ev.Type == termbox.EventKey {
+
+				if ev.Key == termbox.KeyArrowUp {
+					pos.Y += 10
+				}
+				if ev.Key == termbox.KeyArrowDown {
+					pos.Y -= 10
+				}
+				if ev.Key == termbox.KeyArrowRight {
+					pos.X += 10
+				}
+				if ev.Key == termbox.KeyArrowLeft {
+					pos.X -= 10
+				}
+			}
+
+
+		}
 	}
 }
